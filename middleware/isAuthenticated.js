70d66ea3d8b5e4f402694296903c6ee76b3f3c3e
Billal ZERNENOU <billalzernenou@gmail.com>
2021-02-10 21:17:52 +0100

    fiw token issue authenticated

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,9 +4,9 @@ const User = require("../models/User");
 const isAuthenticated = async (req, res, next) => {
   try {
     // grab token authorization headers
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.replace("Bearer ", "");
 
-    const token = req.headers.authorization.replace("Bearer ", "");
-    if (token) {
       //check existence user in BDD
       const user = await User.findOne({ token: token }).select(
         "account email token"
