77ac885dcc30f80b55e03887de058589f2fd592b
Billal ZERNENOU <billalzernenou@gmail.com>
2021-02-10 19:05:46 +0100

    fix Cannot read property 'replace' of undefined case user not authenticated

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,15 +4,19 @@ const User = require("../models/User");
 const isAuthenticated = async (req, res, next) => {
   try {
     // grab token authorization headers
-    const token = req.headers.authorization.replace("Bearer ", "");
-    //check existence user in BDD
-    const user = await User.findOne({ token: token }).select(
-      "account email token"
-    );
-    if (user) {
-      // add key user to req.user
-      req.user = user;
-      return next();
+    if (token) {
+      const token = req.headers.authorization.replace("Bearer ", "");
+      //check existence user in BDD
+      const user = await User.findOne({ token: token }).select(
+        "account email token"
+      );
+      if (user) {
+        // add key user to req.user
+        req.user = user;
+        return next();
+      } else {
+        res.status(401).json({ message: "Unauthorized" });
+      }
     } else {
       res.status(401).json({ message: "Unauthorized" });
     }
